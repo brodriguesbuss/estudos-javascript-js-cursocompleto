84836b297972ca5cf35b27c41e00192d932e9eaa
Brenda Rodrigues <brenda.buss@br.experian.com>
2024-04-14 00:12:18 -0300

    feat/secao2-aula8: metodo construtor e encapsulamento

--- projetoCalculadora/scripts/controller/CalcController.js
+++ projetoCalculadora/scripts/controller/CalcController.js
@@ -1,3 +1,22 @@
 class CalcController {
-    
+  constructor() {
+    this._displayCalc = "0";
+    this._currentDate;
+  }
+
+  get displayCalc() {
+    return this._displayCalc;
+  }
+
+  set displayCalc(valor) {
+    this._displayCalc = valor;
+  }
+
+  get currentDate_currentDate() {
+    return this._currentDate;
+  }
+
+  set currentDate_currentDate(novaData) {
+    this._currentDate = novaData;
+  }
 }
