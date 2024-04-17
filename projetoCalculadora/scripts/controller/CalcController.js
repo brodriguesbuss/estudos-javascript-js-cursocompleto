bed4bfa976b43264100db44dd35d17102a82d1de
Brenda Rodrigues <brenda.buss@br.experian.com>
2024-04-16 23:00:57 -0300

    feat/secao2-aula15: adicionando operacoes - isNaN e Array.length

--- projetoCalculadora/scripts/controller/CalcController.js
+++ projetoCalculadora/scripts/controller/CalcController.js
@@ -30,9 +30,35 @@ class CalcController {
     this._operation.pop();
   }
 
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  setLastOperation(value){
+    this._operation[this._operation.length - 1] = value;
+  }
+
+  isOperator(value) {
+    return ["+", "-", "*", "%", "/"].indexOf(value) > -1;
+  }
+
   addOperation(value) {
+    if (isNaN(this.getLastOperation())) {
+      //é uma string
+      if (this.isOperator(value)) {
+        this.setLastOperation(value);
+      } else if (isNaN(value)) {
+      } else {
+        // é um numero sim
+        this._operation.push(value);
+      }
+    } else {
+      //number
+      let newValue = this.getLastOperation().toString() + value.toString();
+      this.setLastOperation(parseInt(newValue));
+    }
     this._operation.push(value);
-    console.log(this._operation)
+    console.log(this._operation);
   }
   setError() {
     this.displayCalc = "Error";
@@ -47,17 +73,25 @@ class CalcController {
         this.clearEntry();
         break;
       case "soma":
+        this.addOperation("+");
         break;
       case "subtracao":
+        this.addOperation("-");
         break;
       case "divisao":
+        this.addOperation("/");
         break;
       case "multiplicação":
+        this.addOperation("*");
         break;
       case "porcento":
+        this.addOperation("%");
         break;
       case "igual":
+        this.addOperation("=");
         break;
+      case "ponto":
+        this.addOperation(".");
       case "0":
       case "1":
       case "2":
